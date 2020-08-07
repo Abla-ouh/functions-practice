1505a9ca4c2d93d5bc74516471b0c3c72b7646f3
SpiderX <hamza.ezzahiry@gmail.com>
2020-08-07 11:53:13 +0100

    I add a new method + i make a headfile

--- 7 - swap two numbers.c
+++ 7 - swap two numbers.c
@@ -1,14 +1,31 @@
+/******************************************************************************
+                                    ***
+                         *** Functions practice ***
+                                   *****
+    author   : Abla-Ouh
+    Exercice : Write a program in C to swap two numbers using function
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
-void swap(int a , int b){
+void swap(int a , int b)
+{
         int aux;
-        printf("enter a = ");
-        scanf("%d",&a);
-        printf("enter b = ");
-        scanf("%d",&b);
+        printf("enter a = "); scanf("%d",&a);
+        printf("enter b = "); scanf("%d",&b);
+        //======= Method 1 : Using a temp variable (aux) ==============
         aux = a;
         a = b;
         b = aux;
+        //=============================================================
+        
+        /*
+        //======= Method 2 : without a temp variable ========
+            a = a - b;   
+            b = a + b;
+            a = b - a;
+        //==================================================*/
         printf("a = %d\nb = %d",a,b);
 }
 
